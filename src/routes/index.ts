8cee32da193c815c7ec77fa8c0b8311053c7e701
howmarketing <gabsariza@icloud.com>
2024-08-04 16:41:52 -0300

    feat(list-departments): Create the endpoint to list the departments
    # FEATURE LIST DEPARTMENTS
    
    Fetch departments from the database using the @prisma/client.
    
    ## TODO
    
    - [x] Create the GET route to fetch departments as `/departments`;
    - [x] Register the GET route `/departments` in `routes/index.ts`;
    - [x] Test the route ensuring the correct behaviour and response.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import baseRouteRegister from "@routes/baseRoute"
 import usersRouteRegister from "@routes/users"
 import employeesRouteRegister from "@routes/employees"
+import departmentsRouteRegister from "@routes/departments"
 
 console.log("Registering routes...")
 export default () => {
     baseRouteRegister()
     usersRouteRegister()
     employeesRouteRegister()
+    departmentsRouteRegister()
 }
